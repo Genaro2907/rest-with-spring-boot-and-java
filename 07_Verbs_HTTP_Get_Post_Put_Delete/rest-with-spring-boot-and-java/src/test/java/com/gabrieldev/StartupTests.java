1f4e66be3a8b939008d5da5f57afd198fc5cc735
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-10-15 19:59:08 -0300

    Verbs Get, Post, Update e Delete

---
+++ 07_Verbs_HTTP_Get_Post_Put_Delete/rest-with-spring-boot-and-java/src/test/java/com/gabrieldev/StartupTests.java
@@ -0,0 +1,13 @@
+package com.gabrieldev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
