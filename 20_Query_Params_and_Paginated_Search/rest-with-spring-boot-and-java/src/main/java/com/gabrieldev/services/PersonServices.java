2a6006ff68abfb24253b4f5ed0a3ad63a0ebddc4
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-12-27 17:24:30 -0300

    Query Params and Paginated Search

---
+++ 20_Query_Params_and_Paginated_Search/rest-with-spring-boot-and-java/src/main/java/com/gabrieldev/services/PersonServices.java
@@ -0,0 +1,98 @@
+package com.gabrieldev.services;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrieldev.controllers.PersonController;
+import com.gabrieldev.data.vo.v1.PersonVO;
+import com.gabrieldev.exceptions.RequiredObjectIsNullException;
+import com.gabrieldev.exceptions.ResourceNotFoundException;
+import com.gabrieldev.mapper.GabrielMapper;
+import com.gabrieldev.model.Person;
+import com.gabrieldev.repositories.PersonRepository;
+
+import jakarta.transaction.Transactional;
+
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+
+	public List<PersonVO> findAll() {
+
+		logger.info("Finding all people!");
+		var persons = GabrielMapper.parseListObjects(repository.findAll(), PersonVO.class);
+		persons
+			.stream()
+			.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		return persons;
+	}
+
+	public PersonVO findById(Long id) {
+		
+		logger.info("Finding one person!");
+		var entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		var vo = GabrielMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
+	}
+	
+	public PersonVO create(PersonVO person) {
+
+		if (person == null) throw new RequiredObjectIsNullException();
+		logger.info("Creating one person!");
+		var entity = GabrielMapper.parseObject(person, Person.class);
+		var vo =  GabrielMapper.parseObject(repository.save(entity), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+
+		if (person == null) throw new RequiredObjectIsNullException();
+		logger.info("Updating one person!");
+		
+		var entity = repository.findById(person.getKey())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		var vo =  GabrielMapper.parseObject(repository.save(entity), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+	
+	@Transactional
+	public PersonVO disablePerson(Long id) {
+		
+		logger.info("Disabling one person!");
+		repository.disablePerson(id);
+		
+		var entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		var vo = GabrielMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		
+		logger.info("Deleting one person!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+}
