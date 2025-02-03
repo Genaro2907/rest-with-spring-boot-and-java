b007cf24d12b322d820a98db669e96961d1d7375
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-02-03 19:47:55 -0300

    Test

---
+++ 20_Query_Params_and_Paginated_Search/rest-with-spring-boot-and-java/bin/src/main/resources/db/migration/V2__Popalate_Table_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person`(`id`, `address` , `first_name`, `gender` ,`last_name`) VALUES 
+	(1,'Tucuruvi - São Paulo','Gabriel','Male','da Silva'),
+	(2,'Tucuruvi - São Paulo','Gabriel','Male','da Silva'),
+	(3,'Tucuruvi - São Paulo','Gabriel','Male','Genaro');
+UNLOCK TABLES;
