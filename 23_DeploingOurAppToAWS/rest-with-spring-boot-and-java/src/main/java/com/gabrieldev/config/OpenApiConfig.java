4d483ebc286ce9959c03d4c314cd92ff1f120871
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-03-11 17:40:06 -0300

    Continuous Integration with Github, Github Actions, Amazon ECR and ECS - Foo Bar

--- 23_DeploingOurAppToAWS/rest-with-spring-boot-and-java/src/main/java/com/gabrieldev/config/OpenApiConfig.java
+++ 23_DeploingOurAppToAWS/rest-with-spring-boot-and-java/src/main/java/com/gabrieldev/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
 	OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(new Info()
-						.title("RESTful API with Java and SpringBoot 3")
+						.title("RESTful API with Java and SpringBoot 3 - Foo Bar")
 						.version("v1")
 						.description("Curso de SpringBoot")
 						.termsOfService("https://github.com/Genaro2907")
